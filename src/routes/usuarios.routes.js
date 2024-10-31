4a753c1341d9984f881a6dba3a233e2a543b1c50
Bruna Savelli <bnsavelli@gmail.com>
2024-10-31 14:27:28 -0300

    fixed get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,7 +6,11 @@ let usuarios = [];
 
 usuariosRoutes.get("/", (req, res) => {
   return res.status(200).json({
-    message: usuarios.length == 0 ? "Não há usuários cadastrados": `Total de usuários: ${usuarios.length}`
+    message: 
+    usuarios.length == 0 
+    ? "Não há usuários cadastrados"
+    : `Total de usuários: ${usuarios.length}`,
+    usuarios,
   });
 });
 
